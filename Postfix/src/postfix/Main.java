eb5f60d11c13845785a1bb43385a49e1d74b9445
bigyihsuan <bigyihsuan@gmail.com>
2017-10-05 09:32:55 -0400

    Add files via upload

--- Postfix/src/postfix/Main.java
+++ Postfix/src/postfix/Main.java
@@ -7,90 +7,117 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		//declare and init classes and vars
-		Scanner input = new Scanner(new File("numbers.txt"));
+		Scanner input = new Scanner(System.in);
 		Stack<Integer> stack = new Stack<Integer>();
-		
-		String exp = "";	//expression
-		int op1, op2;		//operands
-		int temp1, temp2;
-		
-		while (input.hasNextLine()) {
+
+		String exp = ""; //expression
+		boolean run;
+
+		do {
+			System.out.print("Enter a valid postfix expression: ");
 			exp = input.nextLine();
-			for (int i = 0; i < exp.length(); i++) {
-				//check to see if there are at least 2 elements in the stack
-				System.out.println(stack);
-				switch (exp.charAt(i)) {
-					case '+': {
-						if (stack.size() > 0 && hasTwoElements(stack)) {
-							op2 = stack.pop();
-							op1 = stack.pop();
-							stack.push(op1 + op2);
-							break;
-						}
-					}
-					case '-': {
-						if (stack.size() > 0 && hasTwoElements(stack)) {
-							op2 = stack.pop();
-							op1 = stack.pop();
-							stack.push(op1 - op2);
-							break;
-						}
-					}
-					case '*': {
-						if (stack.size() > 0 && hasTwoElements(stack)) {
-							op2 = stack.pop();
-							op1 = stack.pop();
-							stack.push(op1 * op2);
-							break;
-						}
-					}
-					case '/': {
-						if (stack.size() > 0 && hasTwoElements(stack)) {
-							op2 = stack.pop();
-							op1 = stack.pop();
-							stack.push(op1 / op2);
-							break;
-						}
-					}
-					case '%': {
-						if (stack.size() > 0 && hasTwoElements(stack)) {
-							op2 = stack.pop();
-							op1 = stack.pop();
-							stack.push(op1 % op2);
-							break;
-						}
-					}
-					case '^': {
-						if (stack.size() > 0 && hasTwoElements(stack)) {
-							op2 = stack.pop();
-							op1 = stack.pop();
-							stack.push(op1 ^ op2);
-							break;
-						}
-					}
-					default: {
-						stack.push(exp.charAt(i) - 48);
+			pushOnToStack(exp, stack);
+			
+			if (stack.size() > 1) {
+				System.out.println("Error: Not enough operators");
+			} else {
+				System.out.println(stack.peek());
+				stack.clear();
+			}
+			
+			System.out.print("Run again? (y/n): ");
+			exp = input.nextLine();
+			run = (exp.charAt(0) == 'y') ? true : false;
+		} while (run);
+
+		System.out.println("done");
+	}
+	
+	public static <E> void pushOnToStack(String exp, Stack<Integer> stack) {
+		int op1, op2;		//operands
+		for (int i = 0; i < exp.length(); i++) {
+			//check to see if there are at least 2 elements in the stack
+			boolean thing = stack.size() >= 0;
+			
+			switch (exp.charAt(i)) {
+				case '+': {
+					if (thing && hasTwoElements(stack)) {
+						op2 = stack.pop();
+						op1 = stack.pop();
+						stack.push(op1 + op2);
 						break;
 					}
 				}
+				case '-': {
+					if (thing && hasTwoElements(stack)) {
+						op2 = stack.pop();
+						op1 = stack.pop();
+						stack.push(op1 - op2);
+						break;
+					}
+				}
+				case '*': {
+					if (thing && hasTwoElements(stack)) {
+						op2 = stack.pop();
+						op1 = stack.pop();
+						stack.push(op1 * op2);
+						break;
+					}
+				}
+				case '/': {
+					if (thing && hasTwoElements(stack)) {
+						op2 = stack.pop();
+						op1 = stack.pop();
+						if (op2 == 0) {
+							System.out.println("Error: Divide by 0!");
+						} else {
+							stack.push(op1 / op2);
+						}
+						break;
+					}
+				}
+				case '%': {
+					if (thing && hasTwoElements(stack)) {
+						op2 = stack.pop();
+						op1 = stack.pop();
+						if (op2 == 0) {
+							System.out.println("Error: Divide by 0!");
+						} else {
+							stack.push(op1 % op2);
+						}
+						break;
+					}
+				}
+				case '^': {
+					if (thing && hasTwoElements(stack)) {
+						op2 = stack.pop();
+						op1 = stack.pop();
+						stack.push(op1 ^ op2);
+						break;
+					}
+				}
+				default: {
+					stack.push(exp.charAt(i) - 48);
+					break;
+				}
 			}
+			System.out.println(stack);
 		}
-		
 	}
 	
-	public static <Integer> boolean hasTwoElements(Stack<Integer> stack) {
-		int opCount = 0;
-		Integer temp1 = new Integer(0);
-		Integer temp2 = new Integer(0);
+	public static <E> boolean hasTwoElements(Stack<E> stack) {
+		int temp1 = 0;
+		int temp2 = 0;
 		boolean happened = false;
 		if (!stack.isEmpty()) {
 			//check if the assignment is success
 			happened = ((temp1 = (java.lang.Integer) stack.pop()) != 0)
 					&& ((temp2 = (java.lang.Integer) stack.pop()) != 0);
 			
-			if (happened) {
-				stack.push(temp1);
-				stack.push(temp2);
+			if (happened) { //if the 2 assignments happened
+				stack.push((E) new Integer(temp2)); //reverse order b/c popped in order
+				stack.push((E) new Integer(temp1));
+				return true;
 			}
 		}
 		return false;
